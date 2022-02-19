4c8707ba91e59453bc3e9f644fa128445155c2e6
Gustavo <gustavokkg2@hotmail.com>
2022-02-19 20:33:41 -0300

    dashboard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import Login from './components/Login';
 import './index.css';
 import "bootstrap/dist/css/bootstrap.min.css"
+import { useEffect, useState } from 'react';
+import { getTokenFromResponse } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+  const [token, setToken] = useState(null)
+
+
+  useEffect(() => {
+    const hash = getTokenFromResponse();
+    window.location.hash = "";
+
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token)
+
+      spotify.setAccessToken(_token);
+    }
+  }, []);
+
+
   return (
-    <Login />
+    <div>
+      { token ? (<h1>dashboard</h1>) : (<Login />)}
+    </div>
   );
 }
 
--- src/spotify.js
+++ src/spotify.js
@@ -2,7 +2,6 @@ export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 const redirectUri = 'http://localhost:3000/';
 const clientId = '8d0a83dea9464b8e8ef6452f0facc1ca';
-//const clientSecret = '8e021bad7ea842a1ac1d475bb333a15d';
 
 const scopes = [
     'user-read-recently-played',
@@ -15,4 +14,17 @@ const scopes = [
     'playlist-read-private'
 ];
 
+export const getTokenFromResponse = () => {
+    return window.location.hash
+      .substring(1)
+      .split("&")
+      .reduce((initial, item) => {
+        var parts = item.split("=");
+        initial[parts[0]] = decodeURIComponent(parts[1]);
+  
+        return initial;
+      }, {});
+};
+
 export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=token&show_dialog=true`;
+
